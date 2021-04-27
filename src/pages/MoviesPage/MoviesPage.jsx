ea594997faa8278bd59f26e8698736635e573a21
AritroSinha <sinhaaritro@gmail.com>
2021-04-27 12:28:00 +0530

    Moved useFetch to MovieList. Reduced complexity in MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,27 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 // import PropTypes from "prop-types";
-import useFetch from "library/hooks/useFetch";
-import * as Constants from "library/constants/constants";
 import BottomNavigationBar from "library/layouts/BottomNavigation/BottomNavigationBar";
 import SearchMovies from "./layouts/SearchMovies";
 import MovieList from "./layouts/MovieList";
 
 const MoviesPage = (props) => {
     const [searchText, setSearchText] = useState("");
-    const [url, setUrl] = useState("");
-    const { status, data: movieList } = useFetch(url);
 
     const searchTextChanged = (newSearchText) => setSearchText(newSearchText);
 
     const onSearchClicked = () => setSearchText(searchText);
 
-    useEffect(() => {
-        if (searchText === "") return;
-        setUrl(
-            `${Constants.baseSearchApiURL}?api_key=${process.env.REACT_APP_TMDB_API}&query=${searchText}&page=0}&include_adult=true`
-        );
-    }, [searchText]);
-    console.log(status);
     return (
         <>
             <SearchMovies
@@ -29,11 +18,8 @@ const MoviesPage = (props) => {
                 onSearchClicked={onSearchClicked}
                 searchTextChanged={searchTextChanged}
             />
-            {movieList.results === undefined ? (
-                "Nothing here"
-            ) : (
-                <MovieList list={movieList.results} />
-            )}
+            {!searchText && "Nothing here. Type in search to get results"}
+            {searchText && <MovieList query={searchText} />}
             <BottomNavigationBar />
         </>
     );
--- src/pages/MoviesPage/layouts/MovieList.jsx
+++ src/pages/MoviesPage/layouts/MovieList.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import useFetch from "library/hooks/useFetch";
+import * as Constants from "library/constants/constants";
 import Grid from "@material-ui/core/Grid";
 import MovieCard from "./MovieCard";
 
-const MovieList = ({ list }) => {
+const MovieList = ({ query }) => {
+    const { status, data: movieList } = useFetch(
+        `${Constants.baseSearchApiURL}?api_key=${process.env.REACT_APP_TMDB_API}&query=${query}&page=0}&include_adult=true`
+    );
+
     return (
         <>
-            <Grid container spacing={2}>
-                {list.map((listItem) => {
-                    return <MovieCard key={listItem.id} item={listItem} />;
-                })}
-            </Grid>
+            {movieList.results && (
+                <Grid container spacing={2}>
+                    {movieList.results.map((listItem) => {
+                        return <MovieCard key={listItem.id} item={listItem} />;
+                    })}
+                </Grid>
+            )}
         </>
     );
 };
