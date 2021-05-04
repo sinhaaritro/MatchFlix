29d0b19aa27bb5755c85fd3d4777b565656173bd
AritroSinha <sinhaaritro@gmail.com>
2021-05-04 21:30:48 +0530

    Added tool bar component in BottomNavBar to stop text from going under

--- src/library/layouts/BottomNavigation/BottomNavigationBar.jsx
+++ src/library/layouts/BottomNavigation/BottomNavigationBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 // import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import GroupRoundedIcon from "@material-ui/icons/GroupRounded";
@@ -12,42 +13,45 @@ const BottomNavigationBar = (props) => {
     const [value, setValue] = useState(window.location.pathname);
 
     return (
-        <AppBar
-            component="nav"
-            position="fixed"
-            color="primary"
-            style={{ top: "auto", bottom: 0 }}
-        >
-            <BottomNavigation
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
-                showLabels
+        <>
+            <Toolbar />
+            <AppBar
+                component="nav"
+                position="fixed"
+                color="primary"
+                style={{ top: "auto", bottom: 0 }}
             >
-                <BottomNavigationAction
-                    component={NavLink}
-                    to="/"
-                    value="/"
-                    label="Groups"
-                    icon={<GroupRoundedIcon />}
-                />
-                <BottomNavigationAction
-                    component={NavLink}
-                    to="/movies"
-                    value="/movies"
-                    label="Movies"
-                    icon={<MovieRoundedIcon />}
-                />
-                <BottomNavigationAction
-                    component={NavLink}
-                    to="/profile"
-                    value="/profile"
-                    label="Profile"
-                    icon={<AccountCircleRoundedIcon />}
-                />
-            </BottomNavigation>
-        </AppBar>
+                <BottomNavigation
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                    showLabels
+                >
+                    <BottomNavigationAction
+                        component={NavLink}
+                        to="/"
+                        value="/"
+                        label="Groups"
+                        icon={<GroupRoundedIcon />}
+                    />
+                    <BottomNavigationAction
+                        component={NavLink}
+                        to="/movies"
+                        value="/movies"
+                        label="Movies"
+                        icon={<MovieRoundedIcon />}
+                    />
+                    <BottomNavigationAction
+                        component={NavLink}
+                        to="/profile"
+                        value="/profile"
+                        label="Profile"
+                        icon={<AccountCircleRoundedIcon />}
+                    />
+                </BottomNavigation>
+            </AppBar>
+        </>
     );
 };
 
