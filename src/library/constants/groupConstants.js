0191d5772f511e63984fd9a383a4aca9b0099bd7
AritroSinha <sinhaaritro@gmail.com>
2021-06-20 21:03:37 +0530

    Merged genresList

--- src/library/constants/groupConstants.js
+++ src/library/constants/groupConstants.js
@@ -4,7 +4,6 @@ export const genresList = [
     { id: 28, name: "Action" },
     { id: 16, name: "Animation" },
     { id: 12, name: "Adventure" },
-    { id: 16, name: "Animation" },
     { id: 35, name: "Comedy" },
     { id: 80, name: "Crime" },
     { id: 99, name: "Documentary" },
