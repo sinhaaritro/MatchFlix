9ba3823eb332a6fd8fc70ce448794f9e87eb218f
AritroSinha <sinhaaritro@gmail.com>
2021-06-22 11:41:15 +0530

    Moved useFetch to higher order

--- src/pages/GroupPage/layouts/AllMovieList.jsx
+++ src/pages/GroupPage/layouts/AllMovieList.jsx
@@ -1,39 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
-import { useGroupContext } from "library/provider/Groups/GroupProvider";
-import Typography from "@material-ui/core/Typography";
 import MovieCard from "./MovieCard";
-import useFetch from "library/hooks/useFetch";
-import * as Constants from "library/constants/constants";
 
-const AllMovieList = () => {
-    const { groupState } = useGroupContext();
-    const { status, data: movieData } = useFetch(
-        `/api/tmdbMoviesByIDs`,
-        groupState.allCards
-    );
-
+const AllMovieList = ({ list }) => {
+    console.log(list);
     return (
         <>
-            {status === Constants.apiStatus.LOADING && (
-                <Typography variant="caption" color="textSecondary">
-                    Loading...
-                </Typography>
-            )}
-            {status === Constants.apiStatus.ERROR && (
-                <Typography variant="caption" color="textSecondary">
-                    Error...
-                </Typography>
-            )}
-            {status === Constants.apiStatus.SUCCESS && (
-                <Grid container spacing={2}>
-                    {movieData?.map((movie) => {
-                        return <MovieCard key={movie.id || -1} item={movie} />;
-                    })}
-                </Grid>
-            )}
+            <Grid container spacing={2}>
+                {list?.map((movie) => {
+                    return <MovieCard key={movie.id || -1} item={movie} />;
+                })}
+            </Grid>
         </>
     );
+};
+
+AllMovieList.propTypes = {
+    list: PropTypes.array,
 };
 
 export default AllMovieList;
